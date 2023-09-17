ec55c2e0f6cc3f8a6a0c8b08c84ec8ef2a44c6ed
indira-5 <indu109@gmail.com>
2023-09-17 01:04:27 -0700

    sec

--- IndiraDjango/IndiraDjango/settings.py
+++ IndiraDjango/IndiraDjango/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'IndiraDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DjangoIndiraJuly',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'Localhost',
+        'PORT':'3306',
+
+
     }
 }
 
